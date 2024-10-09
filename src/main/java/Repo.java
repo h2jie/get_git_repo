f90e6ed67b10373685c7ef0902f0783749087f78
Hangjie Huang <h2jie09@gmail.com>
2024-10-09 23:31:16 +0200

    Use Gson to Processing data

--- src/main/java/Repo.java
+++ src/main/java/Repo.java
@@ -3,25 +3,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 //@JsonIgnoreProperties(ignoreUnknown = true) // Ignore unrecognized attributes
 public class Repo {
-//    @JsonProperty("id")
-//    private int id;
-//
-//    @JsonProperty("name")
-//    private String name;
-//
-//    @JsonProperty("description")
-//    private String description;
-//
-//    @JsonProperty("stargazers_count")
-//    private int stargazers_count;
-//
-//    @Override
-//    public String toString() {
-//        return "Repo Name: " + name +
-//                "\nDescription: " + (description != null ? description : "No description") +
-//                "\nStars: " + stargazers_count + "\n";
-//    }
-String name;
+
+    String name;
     String description;
     int stargazers_count;
 
